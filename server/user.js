12f83b9f70565e866b7ce4e35a01cc047039397c
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-03-19 11:58:14 -0700

    filled out all of the user creation logic

--- server/user.js
+++ server/user.js
@@ -1,3 +1,5 @@
+const uuid = require('uuid');
+
 const { getDb } = require('./db');
 
 class User {
@@ -5,15 +7,38 @@ class User {
         this.name = name;
         this.created = Date.now();
         this.isVerified = false;
+        this.accessToken = uuid();
     }
 }
 
 async function registerUser(req, res) {
     const db = await getDb(process.env.DB_URL);
     const collection = await db.collection('users');
-    const result = await collection.insertOne(new User(req.body.name));
-    console.log(result);
-    res.sendStatus(200);
+
+    try {
+        const users = await collection.find({ name: req.body.name }).toArray();
+        if (users.length === 0) {
+            const { result } = await collection.insertOne(new User(req.body.name));
+            if (result.ok === 1) {
+                res.sendStatus(201);
+            }
+            else {
+                res.status(500).json({
+                    status: 'error',
+                    message: 'something went wrong'
+                });
+            }
+        }
+        else {
+            res.status(400).json({
+                status: 'error',
+                message: 'user already exists'
+            });
+        }
+    }
+    catch (e) {
+        res.status(500).json(e);
+    }
 }
 
 module.exports = {
