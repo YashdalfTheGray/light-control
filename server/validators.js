c16ca4b310be8a879aa5d7a0b99c22b4a68cd7c3
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-03-22 22:34:26 -0700

    added some request validator middlewares to the app

---
+++ server/validators.js
@@ -0,0 +1,34 @@
+const { get } = require('lodash');
+
+function validateRequestBody(key) {
+    return (req, res, next) => {
+        if (!get(req.body, key)) {
+            res.status(400).json({
+                status: 'error',
+                message: 'malformed request body'
+            });
+        }
+        else {
+            next();
+        }
+    }
+}
+
+function validateQueryParam(key) {
+    return (req, res, next) => {
+        if (!get(req.query, key)) {
+            res.status(400).json({
+                status: 'error',
+                message: 'request missing query parameters'
+            });
+        }
+        else {
+            next();
+        }
+    }
+}
+
+module.exports = {
+    validateRequestBody,
+    validateQueryParam
+};
