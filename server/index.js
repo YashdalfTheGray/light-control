a5c679ca37414d3fbb77150f62e7fda91c6280a9
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-03-24 20:03:08 -0700

    removed a bunch of admin code from server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-require('./init')('../admins.json');
 
 const express = require('express');
 const morgan = require('morgan');
@@ -7,9 +6,9 @@ const bodyParser = require('body-parser');
 const chalk = require('chalk');
 
 const { closeDb } = require('./db');
-const { authenticateAsUser, authenticateAsAdmin } = require('./auth');
+const { authenticate } = require('./auth');
 const { validateRequestBody, validateQueryParam } = require('./validators');
-const { registerUser, loginUser, getUsers, deleteUser, verifyUser } = require('./user');
+const { registerUser, loginUser, deleteUser } = require('./user');
 
 const port = process.env.PORT || process.argv[2] || 8080;
 const wrap = fn => (...args) => fn(...args).catch(args[2]);
@@ -27,11 +26,9 @@ app.get('/', (req, res) => {
 
 app.post('/users/register', validateRequestBody('name'), wrap(registerUser));
 app.get('/users/login', validateQueryParam('name'), wrap(loginUser));
-app.post('/users/verify', validateRequestBody('name'), wrap(authenticateAsAdmin), wrap(verifyUser));
-app.get('/users', wrap(authenticateAsAdmin), wrap(getUsers));
-app.delete('/users', validateRequestBody('name'), wrap(authenticateAsAdmin), wrap(deleteUser));
+app.delete('/users', validateRequestBody('name'), wrap(authenticate), wrap(deleteUser));
 
-app.post('/lights/:room', wrap(authenticateAsUser), (req, res) => res.send('you\'re in!'));
+app.post('/lights/:room', wrap(authenticate), (req, res) => res.send('you\'re in!'));
 
 app.listen(port, () => console.log(`Server running on port ${chalk.green(port)}`));
 
