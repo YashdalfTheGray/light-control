97b5d5190c1b55e7c29d162cb5d68bd301b3deef
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-04-04 21:22:50 -0700

    app component is now stateless

--- app/components/AppComponent.tsx
+++ app/components/AppComponent.tsx
@@ -2,23 +2,24 @@ import * as React from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 
-export default class AppComponent extends React.Component<{}, {}> {
-    render() {
-        const moreButton = (
-            <IconButton
-                iconClassName="material-icons"
-                tooltip="More">
-                more_vert
-            </IconButton>
-        );
+import LoginPage from './LoginPage';
 
-        return (
-            <div>
-                <AppBar
-                    title="Light Control"
-                    iconElementRight={moreButton}
-                    showMenuIconButton={false} />
-            </div>
-        );
-    }
+export default function AppComponent() {
+    const moreButton = (
+        <IconButton
+            iconClassName="material-icons"
+            tooltip="More">
+            more_vert
+        </IconButton>
+    );
+
+    return (
+        <div>
+            <AppBar
+                title="Light Control"
+                iconElementRight={moreButton}
+                showMenuIconButton={false} />
+            <LoginPage />
+        </div>
+    );
 }
