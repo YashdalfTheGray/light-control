baa008c742791f6e96d7ac638470a4d3be4d6b7e
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-04-06 19:47:35 -0700

    wired up a snackbar for the login success case

--- app/components/LoginPage.tsx
+++ app/components/LoginPage.tsx
@@ -2,10 +2,13 @@ import * as React from 'react';
 import { Card, CardActions, CardTitle, CardText } from 'material-ui/Card';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
+import Snackbar from 'material-ui/Snackbar';
 
 class LoginPageState {
     name?: string;
     token?: string;
+    showLoginSnackbar?: boolean;
+    loginMessage?: string;
 }
 
 export default class LoginPage extends React.Component<{}, LoginPageState> {
@@ -14,11 +17,14 @@ export default class LoginPage extends React.Component<{}, LoginPageState> {
 
         this.state = {
             name: '',
-            token: ''
+            token: '',
+            showLoginSnackbar: false,
+            loginMessage: ''
         };
 
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleLogin = this.handleLogin.bind(this);
+        this.handleRequestClose = this.handleRequestClose.bind(this);
     }
 
     handleNameChange({ currentTarget: { value } }:React.FormEvent<HTMLInputElement>) {
@@ -27,12 +33,21 @@ export default class LoginPage extends React.Component<{}, LoginPageState> {
         });
     }
 
+    handleRequestClose() {
+        this.setState({
+            showLoginSnackbar: false
+        });
+    }
+
     async handleLogin() {
         try {
             const response = await fetch(`/api/users/login?name=${this.state.name}`);
+            console.log(response);
             const resBody = await response.json();
             this.setState({
-                token: resBody.token
+                token: resBody.token,
+                showLoginSnackbar: true,
+                loginMessage: 'Login successful'
             });
         }
         catch(e) {
@@ -42,22 +57,29 @@ export default class LoginPage extends React.Component<{}, LoginPageState> {
 
     render() {
         return (
-            <Card style={{ margin: '16px' }}>
-                <CardTitle title="Login" />
-                <CardText>
-                    <TextField
-                        floatingLabelText="Name"
-                        fullWidth={true}
-                        onChange={this.handleNameChange} />
-                </CardText>
-                <CardActions>
-                    <FlatButton label="Register" />
-                    <FlatButton
-                        label="Login"
-                        primary={true}
-                        onTouchTap={this.handleLogin} />
-                </CardActions>
-            </Card>
+            <div>
+                <Card style={{ margin: '16px' }}>
+                    <CardTitle title="Login" />
+                    <CardText>
+                        <TextField
+                            floatingLabelText="Name"
+                            fullWidth={true}
+                            onChange={this.handleNameChange} />
+                    </CardText>
+                    <CardActions>
+                        <FlatButton label="Register" />
+                        <FlatButton
+                            label="Login"
+                            primary={true}
+                            onTouchTap={this.handleLogin} />
+                    </CardActions>
+                </Card>
+                <Snackbar
+                    open={this.state.showLoginSnackbar}
+                    message={this.state.loginMessage}
+                    autoHideDuration={4000}
+                    onRequestClose={this.handleRequestClose} />
+            </div>
         );
     }
 }
