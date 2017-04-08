58b3bbe82f9b35b1b93cbe4bc80e25cae62b0018
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-04-07 20:55:51 -0700

    hooked up that confusing as ALL fuck react router

--- app/components/AppComponent.tsx
+++ app/components/AppComponent.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Link, withRouter } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 
 import LoginPage from './LoginPage';
+import LightsPage from './LightsPage';
 
 export default function AppComponent() {
     const moreButton = (
@@ -19,7 +21,12 @@ export default function AppComponent() {
                 title="Light Control"
                 iconElementRight={moreButton}
                 showMenuIconButton={false} />
-            <LoginPage />
+            <Router>
+                <div>
+                    <Route exact path="/" component={LoginPage} />
+                    <Route path="/lights" component={LightsPage} />
+                </div>
+            </Router>
         </div>
     );
 }
