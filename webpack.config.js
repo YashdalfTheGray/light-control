cfc3d9bc671c0f43e6268077a7f41092cad9efab
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-04-14 18:37:20 -0700

    ignore anything in node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
         rules: [
             {
                 test: /\.jsx?$/,
-                use: ['babel-loader']
+                use: ['babel-loader'],
+                exclude: /node_modules/
             }
         ]
     }
