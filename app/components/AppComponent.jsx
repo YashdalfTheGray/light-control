2b9e93c01064e2eba94caf6747537e6af177d07b
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-04-16 19:22:24 -0700

    added a kind of component for the menu

--- app/components/AppComponent.jsx
+++ app/components/AppComponent.jsx
@@ -1,25 +1,17 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
 
 import LoginPage from './LoginPage';
 import LightsPage from './LightsPage';
+import MoreMenu from './MoreMenu';
 
 export default function AppComponent() {
-    const moreButton = (
-        <IconButton
-            iconClassName="material-icons"
-            tooltip="More">
-            more_vert
-        </IconButton>
-    );
-
     return (
         <div>
             <AppBar
                 title="Light Control"
-                iconElementRight={moreButton}
+                iconElementRight={MoreMenu}
                 showMenuIconButton={false} />
             <Router>
                 <div>
