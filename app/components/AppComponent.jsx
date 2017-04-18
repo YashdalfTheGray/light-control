e61312e8135f720afe2e397bcf7ea2b6e86df3d1
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-04-17 20:24:51 -0700

    added the menu back to app component, think it will need to be a stateful component

--- app/components/AppComponent.jsx
+++ app/components/AppComponent.jsx
@@ -1,17 +1,48 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
 
 import LoginPage from './LoginPage';
 import LightsPage from './LightsPage';
-import MoreMenu from './MoreMenu';
+
+function isUserLoggedIn() {
+    return !!sessionStorage.getItem('userToken');
+}
+
+async function handleMenuChange(event) {
+    console.log(event.currentTarget.innerText, isUserLoggedIn());
+}
+
+const moreButton = (
+    <IconButton
+        iconClassName="material-icons"
+        tooltip="More">
+        more_vert
+    </IconButton>
+);
+
+const moreMenu = (
+    <IconMenu
+        iconButtonElement={moreButton}
+        onItemTouchTap={handleMenuChange}
+        targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}>
+        <MenuItem
+            primaryText="Delete Account" />
+        <MenuItem
+            primaryText="Logout" />
+    </IconMenu>
+);
 
 export default function AppComponent() {
     return (
         <div>
             <AppBar
                 title="Light Control"
-                iconElementRight={MoreMenu}
+                iconElementRight={moreMenu}
                 showMenuIconButton={false} />
             <Router>
                 <div>
