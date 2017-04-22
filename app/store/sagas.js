1e41145e99ea47707b47ac3d71d98114a817b6ed
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-04-21 21:51:55 -0700

    created a saga out of loginUser

--- app/store/sagas.js
+++ app/store/sagas.js
@@ -1,3 +1,26 @@
-export default function* helloSaga() {
-    yield console.log('Hello sagas!');
+import { delay } from 'redux-saga';
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
+
+import loginUser from './api';
+
+export function* helloSaga({ data }) {
+    yield delay(5000);
+    yield put({ type: 'TEST_ACTION', data: data });
+}
+
+export function* loginAsync({ data }) {
+    try {
+        const { token } = yield call(loginUser, data);
+        yield put({ type: 'LOGIN_SUCCESSFUL', data: token });
+    }
+    catch (e) {
+        yield put({ type: 'LOGIN_FAILED', data: e.message });
+    }
+}
+
+export default function* rootSaga() {
+    yield [
+        takeEvery('SAY_HELLO', helloSaga),
+        takeLatest('LOGIN_REQUESTED', loginAsync)
+    ];
 }
