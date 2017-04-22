775f77a8d9ed3499252cd689edd49f4ace6472c7
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-04-21 21:52:27 -0700

    added some app pertinent states to the reducer

--- app/store/appReducer.js
+++ app/store/appReducer.js
@@ -3,19 +3,22 @@ import { defaults } from 'lodash';
 const initialState = {
     userName: '',
     userToken: '',
-    isUserLoggedIn: false,
+    loggingIn: false,
+    showSnackbar: false,
+    snackbarMessage: '',
     rooms: []
 };
 
-function appReducer(state = initialState, action) {
-    switch (action.type) {
-    case 'ADD_KEY':
-        return {
-            [action.data]: '',
-            ...state
-        };
-    case 'SET_DEFAULT_VALUE':
-        return defaults(action.data, state);
+function appReducer(state = initialState, { type, data }) {
+    switch (type) {
+    case 'LOGIN_REQUESTED':
+        return defaults({ loggingIn: true }, state);
+    case 'LOGIN_SUCCESSFUL':
+        return defaults({ loggingIn: false, userToken: data }, state);
+    case 'LOGIN_FAILED':
+        return defaults({ loggingIn: false, showSnackbar: true, snackbarMessage: data }, state);
+    case 'TEST_ACTION':
+        return defaults({ testVal: data }, state);
     default:
         return state;
     }
