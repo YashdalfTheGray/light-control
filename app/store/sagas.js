f7706e02e0ef3248740d9ac64c9ff7b6e366913c
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-04-23 16:04:46 -0700

    added a saga for getting all the rooms

--- app/store/sagas.js
+++ app/store/sagas.js
@@ -1,8 +1,8 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
 
-import loginUser from './api';
+import { loginUser, getRooms } from './api';
 
-export function* loginAsync({ data }) {
+export function* loginSaga({ data }) {
     try {
         const { token } = yield call(loginUser, data);
         yield put({ type: 'LOGIN_SUCCESSFUL', data: token });
@@ -12,8 +12,19 @@ export function* loginAsync({ data }) {
     }
 }
 
+export function* getRoomsSaga({ data }) {
+    try {
+        const rooms = yield call(getRooms, data);
+        yield put({ type: 'GET_ROOMS_SUCCESSFUL', data: rooms });
+    }
+    catch (e) {
+        yield put({ type: 'GET_ROOMS_FAILED', data: e.message });
+    }
+}
+
 export default function* rootSaga() {
     yield [
-        takeLatest('LOGIN_REQUESTED', loginAsync)
+        takeLatest('LOGIN_REQUESTED', loginSaga),
+        takeEvery('GET_ROOMS_REQUESTED', getRoomsSaga)
     ];
 }
