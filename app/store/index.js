2af914237186ebc690b2c19de0051939bda2e24b
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-04-23 16:05:33 -0700

    added a get rooms action and login now uses the username out of app state

--- app/store/index.js
+++ app/store/index.js
@@ -10,13 +10,14 @@ const appStore = createStore(appReducer, applyMiddleware(sagaMiddleware));
 
 const actions = {
     setUser: user => appStore.dispatch({ type: 'SET_USER', data: user }),
-    loginUser: user => appStore.dispatch({ type: 'LOGIN_REQUESTED', data: user }),
-    setMessage: msg => appStore.dispatch({ type: 'SET_MESSAGE', data: msg })
+    loginUser: () => appStore.dispatch({ type: 'LOGIN_REQUESTED', data: appStore.getState().user }),
+    setMessage: msg => appStore.dispatch({ type: 'SET_MESSAGE', data: msg }),
+    getRooms: apiKey => appStore.dispatch({ type: 'GET_ROOMS_REQUESTED', data: apiKey })
 };
 
 sagaMiddleware.run(rootSaga);
 
-export default {
+export {
     appStore,
     actions
 };
