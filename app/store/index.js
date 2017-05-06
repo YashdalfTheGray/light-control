ac53de4df73fd5eebf09199e94da99ce41500f28
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-05-05 21:17:40 -0700

    added an action to logout the user

--- app/store/index.js
+++ app/store/index.js
@@ -12,6 +12,7 @@ const actions = {
     setUser: user => appStore.dispatch({ type: 'SET_USER', data: user.toLowerCase() }),
     loginUser: () => appStore.dispatch({ type: 'LOGIN_REQUESTED', data: appStore.getState().user }),
     registerUser: () => appStore.dispatch({ type: 'REGISTRATION_REQUESTED', data: appStore.getState().user }),
+    logoutUser: () => appStore.dispatch({ type: 'LOGOUT_USER' }),
     setMessage: msg => appStore.dispatch({ type: 'SET_MESSAGE', data: msg }),
     clearMessage: () => appStore.dispatch({ type: 'SET_MESSAGE', data: '' }),
     getRooms: apiKey => appStore.dispatch({ type: 'GET_ROOMS_REQUESTED', data: apiKey }),
