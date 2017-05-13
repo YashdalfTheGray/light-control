5a71d9097025dfa493222e68a9649164243a49a1
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-05-13 10:07:39 -0700

    thank god for redux and sagas

--- app/components/Room.jsx
+++ app/components/Room.jsx
@@ -2,27 +2,14 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { Card, CardActions, CardTitle, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
-import { assign } from 'lodash';
 
-import { appStore } from '../store';
+import { appStore, actions } from '../store';
 import RoomLight from './RoomLight';
-
-const getOneLightState = async id => fetch(`/api/lights/${id}`, {
-    headers: {
-        Authorization: `Bearer ${appStore.getState().userToken}`,
-        'Content-Type': 'application/json'
-    }
-});
-
-const getAllLightStates = async (lightIds) => {
-    const responses = await Promise.all(lightIds.map(i => getOneLightState(i)));
-    const lightStates = await Promise.all(responses.map(r => r.json()));
-    return lightStates.reduce((acc, { id, state: { on } }) => ({ ...acc, [id]: on }), {});
-};
 
 export default class Room extends React.Component {
     static propTypes = {
         name: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired,
         lightIds: PropTypes.array.isRequired
     };
 
@@ -30,32 +17,26 @@ export default class Room extends React.Component {
         super(props);
 
         this.state = {
-            showSnackbar: false,
             expanded: false,
-            snackbarMessage: '',
             lights: []
         };
 
-        this.changeRoomState = this.changeRoomState.bind(this);
+        this.unsubscribe = appStore.subscribe(() => {
+            const { lights } = appStore.getState();
+            this.setState({ lights });
+        });
+
         this.turnLightsOn = this.turnLightsOn.bind(this);
         this.turnLightsOff = this.turnLightsOff.bind(this);
         this.handleExpandChange = this.handleExpandChange.bind(this);
     }
 
-    async componentWillMount() {
-        try {
-            const lightStates = await getAllLightStates(this.props.lightIds);
-            this.setState({
-                lights: lightStates
-            });
-        }
-        catch (e) {
-            console.log(e);
-            this.setState({
-                showSnackbar: true,
-                snackbarMessage: 'Could not get light details'
-            });
-        }
+    componentWillMount() {
+        actions.getLightStates(appStore.getState().userToken, ...this.props.lightIds);
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
     }
 
     handleExpandChange(newState) {
@@ -64,59 +45,16 @@ export default class Room extends React.Component {
         });
     }
 
-    async changeRoomState(state) {
-        try {
-            await fetch(`/api/rooms/${this.props.id}`, {
-                method: 'post',
-                headers: {
-                    Authorization: `Bearer ${appStore.getState().userToken}`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(state)
-            });
-            const lightStates = await getAllLightStates(this.props.lightIds);
-            this.setState({
-                lights: lightStates
-            });
-        }
-        catch (e) {
-            this.setState({
-                showSnackbar: true,
-                snackbarMessage: `Cannot turn lights ${state.on ? 'on' : 'off'}`
-            });
-        }
+    changeOneLight(id) {
+        console.log(`Changing state for ${id} still needs implemented`);
     }
 
-    async changeOneLight(id) {
-        try {
-            await fetch(`api/lights/${id}`, {
-                method: 'post',
-                headers: {
-                    Authorization: `Bearer ${appStore.getState().userToken}`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ on: !this.state.lights[id] })
-            });
-            const response = await getOneLightState(id);
-            const { id: lightId, state: { on } } = await response.json();
-            this.setState({
-                lights: assign({}, this.state.lights, { [lightId]: on })
-            });
-        }
-        catch (e) {
-            this.setState({
-                showSnackbar: true,
-                snackbarMessage: 'Cannot change light state'
-            });
-        }
+    turnLightsOn() {
+        actions.setOneRoom(appStore.getState().userToken, this.props.id, { on: true });
     }
 
-    async turnLightsOn() {
-        return this.changeRoomState({ on: true });
-    }
-
-    async turnLightsOff() {
-        return this.changeRoomState({ on: false });
+    turnLightsOff() {
+        actions.setOneRoom(appStore.getState().userToken, this.props.id, { on: false });
     }
 
     render() {
