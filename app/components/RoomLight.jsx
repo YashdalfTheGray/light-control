b94176124ba9989ecaa794fa7170c93c39b741dd
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-05-14 14:37:21 -0700

    pass the state onto the light and changed lights from array to object

--- app/components/RoomLight.jsx
+++ app/components/RoomLight.jsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
+import { grey400, grey900 } from 'material-ui/styles/colors';
 import IconButton from 'material-ui/IconButton';
 
 const RoomLight = props => (
     <IconButton
+        iconStyle={{ color: props.lightState ? grey900 : grey400 }}
         iconClassName="material-icons"
         onTouchTap={props.toggleState}>
         lightbulb_outline
@@ -11,7 +13,8 @@ const RoomLight = props => (
 );
 
 RoomLight.propTypes = {
-    toggleState: PropTypes.func.isRequired
+    toggleState: PropTypes.func.isRequired,
+    lightState: PropTypes.bool.isRequired
 };
 
 export default RoomLight;
