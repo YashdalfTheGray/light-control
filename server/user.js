69ca8905b67592c7f5eb1f4f561a97093ecb2055
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-05-23 20:24:10 -0700

    finished lighting up events for user endpoints

--- server/user.js
+++ server/user.js
@@ -2,6 +2,7 @@ const uuid = require('uuid');
 const { sign } = require('jsonwebtoken');
 
 const { getDb } = require('./db');
+const { logError, logUserRegistration, logUserLogin, logUserDeleted } = require('./events');
 
 class User {
     constructor(name) {
@@ -27,20 +28,25 @@ async function registerUser(req, res) {
             const { result } = await collection.insertOne(new User(req.body.name));
             if (result.ok === 1) {
                 res.sendStatus(201);
+                logUserRegistration(req.body.name, 'successful');
             }
             else {
-                res.status(500).json({
+                const error = {
                     status: 'error',
                     message: 'something went wrong'
-                });
+                };
+                res.status(500).json(error);
+                logError(error);
             }
         }
         else {
             res.sendStatus(400);
+            logUserRegistration(req.body.name, 'already_registered');
         }
     }
     catch (e) {
         res.status(500).json(e);
+        logError(e);
     }
 }
 
@@ -54,24 +60,29 @@ async function loginUser(req, res) {
         }).toArray();
 
         if (rest.length !== 0) {
-            res.status(500).json({
+            const error = {
                 status: 'error',
                 message: 'more than one user found'
-            });
+            };
+            res.status(500).json(error);
+            logError(error);
             return;
         }
 
         if (!user) {
             res.sendStatus(403);
+            logUserLogin(req.query.name, 'unauthorized');
         }
         else {
             res.json({
                 token: sign({ usr: user.name, tkn: user.accessToken }, process.env.JWT_SECRET, jwtOptions)
             });
+            logUserLogin(req.query.name, 'successful');
         }
     }
     catch (e) {
         res.status(500).json(e);
+        logError(e);
     }
 }
 
@@ -80,9 +91,11 @@ async function deleteUser(req, res) {
         const db = await getDb(process.env.DB_URL);
         const collection = await db.collection('users');
         res.json(await collection.remove({ name: req.params.name }));
+        logUserDeleted(req.params.name);
     }
     catch (e) {
         res.status(500).json(e);
+        logError(e);
     }
 }
 
