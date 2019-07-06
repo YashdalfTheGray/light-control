2b43782e721fe77eb0da25835e7c812fb05b7dbe
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-07-06 00:13:40 -0400

    added css baseline and removed inject tap plugin or whatever

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import AppComponent from './components/AppComponent';
 
 const App = () => (
-  <MuiThemeProvider>
+  <>
+    <CssBaseline />
     <AppComponent />
-  </MuiThemeProvider>
+  </>
 );
-
-injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.querySelector('#app'));
